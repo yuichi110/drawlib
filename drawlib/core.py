e8db68ba01596d5cfcdeb56aa56370b20811a6cc
yuichi ito <yito86@gmail.com>
2024-03-17 20:54:43 +0900

    add image

--- drawlib/core.py
+++ drawlib/core.py
@@ -1,35 +1,117 @@
-from typing import Any, Final, Union, List, Tuple
+from typing import Final, Union, Optional, List, Tuple, Dict, Any, Literal
+import matplotlib.font_manager
 import matplotlib.pyplot as pyplot
 import matplotlib.patches as patches
+import matplotlib.offsetbox as offsetbox
+import PIL.Image
+import numpy
+
+
+matplotlib.font_manager.FontProperties()
+
+
+class FontStyle:
+
+    def __init__(
+        self,
+        family: Optional[str] = None,
+        file: Optional[str] = None,
+        size: Union[float, str, None] = None,
+        style: Optional[Literal["normal", "italic", "oblique"]] = None,
+        variant: Optional[Literal["normal", "small-caps"]] = None,
+        weight: Union[int, str, None] = None,
+        stretch: Union[int, str, None] = None,
+        math_fontfamily: Optional[str] = None,
+    ):
+        self.family = family
+        self.file = file
+        self.size = size
+        self.style = style
+        self.variant = variant
+        self.weight = weight
+        self.stretch = stretch
+        self.math_fontfamily = math_fontfamily
+
+
+class LineStyle:
+
+    def __init__(self, width, color, style, alpha):
+        self.width = width
+        self.color = color
+        self.style = style
+        self.alpha = alpha
+
+
+class ShapeStyle:
+
+    def __init__(self, lwidth, lcolor, lstyle, lalpha, fcolor, falpha):
+        self.lwidth = lwidth
+        self.lcolor = lcolor
+        self.lstyle = lstyle
+        self.lalpha = lalpha
+        self.fcolor = fcolor
+        self.falpha = falpha
 
 
 class __Drawer:
-    DEFAULT_CONFIG_WIDTH: Final[float] = -1.0
-    DEFAULT_CONFIG_HEIGHT: Final[float] = -1.0
-    DEFUALT_CONFIG_ASPECT: Final[str] = "equal"
-    DEFAULT_CONFIG_AXIS: Final[bool] = False
-    DEFAULT_CONFIG_LOGLEVEL: Final[str] = "info"
+    DEFAULT_WIDTH: Final[float] = 100
+    DEFAULT_HEIGHT: Final[float] = 100
+    DEFUALT_ASPECT: Final[str] = "equal"
+    DEFAULT_AXIS: Final[bool] = False
+    DEFAULT_LOGLEVEL: Final[str] = "info"
+    SYSTEM_FONTS = ["serif", "sanserif"]
+
+    class Title:
+        def __init__(
+            self,
+            s: str,
+            x: Optional[float] = None,
+            y: Optional[float] = None,
+        ):
+            self.s = s
+            self.x = x
+            self.y = y
 
     class Text:
-        def __init__(self, x: float, y: float, s: str):
+        def __init__(
+            self,
+            x: float,
+            y: float,
+            s: str,
+            options: Dict[str, Any],
+        ):
             self.x = x
             self.y = y
             self.s = s
+            self.options = options
 
     class Line:
-        def __init__(self, xs: List[float], ys: List[float]):
+        def __init__(
+            self,
+            xs: List[float],
+            ys: List[float],
+        ):
             self.xs = xs
             self.ys = ys
 
     def __init__(self):
-        self.width = self.DEFAULT_CONFIG_WIDTH
-        self.height = self.DEFAULT_CONFIG_HEIGHT
-        self.aspect = self.DEFUALT_CONFIG_ASPECT
-        self.axis = self.DEFAULT_CONFIG_AXIS
-        self.loglevel = self.DEFAULT_CONFIG_LOGLEVEL
+        self.width = self.DEFAULT_WIDTH
+        self.height = self.DEFAULT_HEIGHT
+        self.aspect = self.DEFUALT_ASPECT
+        self.axis = self.DEFAULT_AXIS
+        self.loglevel = self.DEFAULT_LOGLEVEL
 
+        self.title_: Optional[self.Title] = None
         self._logger = None
-        self._shapes: List[Union[patches.Patch, self.Text, self.Line]] = []
+        self._shapes: List[
+            Union[
+                patches.Patch,
+                self.Text,
+                self.Line,
+                offsetbox.AnnotationBbox,
+            ]
+        ] = []
+        self._image_cache: dict[Union[str, PIL.Image.Image], numpy.array] = {}
 
     ##############
     ### basics ###
@@ -60,7 +142,7 @@ class __Drawer:
 
     def save(self, file: str):
         self._render()
-        pyplot.savefig(file)
+        pyplot.savefig(file, bbox_inches="tight", pad_inches=0)
 
     def _render(self):
         fig = pyplot.figure()
@@ -80,6 +162,19 @@ class __Drawer:
                 ax.text(shape.x, shape.y, shape.s)
             elif isinstance(shape, self.Line):
                 ax.plot(shape.xs, shape.ys)
+            elif isinstance(shape, offsetbox.AnnotationBbox):
+                ax.add_artist(shape)
+
+        if self.title_ is not None:
+            t = self.title_
+            d = {}
+            if t.x:
+                d["x"] = t.x
+            if t.y:
+                d["y"] = t.y
+            ax.set_title(t.s, **d)
+
+        fig.tight_layout()
 
     ####################################
     ### wrapper of matplotlib.patchs ###
@@ -99,8 +194,8 @@ class __Drawer:
 
     def arrow_fancy(self): ...
 
-    def circle(self, x: float, y: float, radius: float, **kwargs: Any):
-        self._shapes.append(patches.Circle((x, y), radius, **kwargs))
+    def circle(self, x: float, y: float, radius: float):
+        self._shapes.append(patches.Circle((x, y), radius))
 
     def ellipse(self): ...
 
@@ -120,15 +215,62 @@ class __Drawer:
     ### original ###
     ################
 
-    def text(self, x: float, y: float, s: str):
-        self._shapes.append(self.Text(x, y, s))
+    def title(
+        self,
+        s: str,
+        x: Optional[float] = None,
+        y: Optional[float] = None,
+        style: Optional[FontStyle] = None,
+    ):
+        self.title_ = self.Title(s, x, y)
 
-    def image(self): ...
+    def text(
+        self,
+        x: float,
+        y: float,
+        s: str,
+        style: Optional[FontStyle] = None,
+    ):
+        options = {}
+        if style is not None:
+            options["fontproperties"] = style
+
+        self._shapes.append(self.Text(x, y, s, options))
+
+    def text_vertical(self, x: float, y: float, s: str): ...
+
+    def image(
+        self,
+        x: float,
+        y: float,
+        file: Optional[str] = None,
+        pilimg: Optional[PIL.Image.Image] = None,
+        zoom=1,
+    ):
+        if file is None and pilimg is None:
+            raise ValueError()
+        if file and pilimg:
+            raise ValueError()
+
+        if file:
+            if file not in self._image_cache:
+                im = pyplot.imread(file)
+                self._image_cache[file] = im
+            else:
+                im = self._image_cache[file]
+        else:
+            key = str(pilimg)
+            if key not in self._image_cache:
+                im = numpy.array(pilimg)
+                self._image_cache[key] = im
+            im = self._image_cache[key]
+
+        imagebox = offsetbox.OffsetImage(im, zoom=zoom)
+        ab = offsetbox.AnnotationBbox(imagebox, (x, y), frameon=False)
+        self._shapes.append(ab)
 
     def line(self, x1: float, y1: float, x2: float, y2: float):
-        xs = [x1, x2]
-        ys = [y1, y2]
-        self._shapes.append(self.Line(xs, ys))
+        self._shapes.append(self.Line([x1, x2], [y1, y2]))
 
     def lines(self, xys: List[Tuple[float, float]]):
         xs = [xy[0] for xy in xys]
@@ -137,12 +279,13 @@ class __Drawer:
 
     def line_bezier(
         self,
+        x: float,
+        y: float,
         bezier_points: List[
             Union[
                 Tuple[float, float],
                 Tuple[float, float, float, float],
-                Tuple[float, float, float, float, float, float],
-            ],
+            ]
         ],
         smooth_points: int = 100,
     ):
@@ -155,33 +298,25 @@ class __Drawer:
                 points.append((x, y))
             return zip(*points)
 
-        xs = []
-        ys = []
-        for i, p in enumerate(bezier_points):
-            length = len(p)
-            if i == 0:
-                if length == 4:
-                    xs.extend([p[0], p[2]])
-                    ys.extend([p[1], p[3]])
-                elif length == 6:
-                    bx, by = quadratic_bezier(p[0], p[1], p[2], p[3], p[4], p[5])
-                    xs.extend(bx)
-                    ys.extend(by)
-                else:
-                    raise ValueError()
-                continue
-            if length == 2:
+        xs = [x]
+        ys = [y]
+        for p in bezier_points:
+            if len(p) == 2:
                 xs.append(p[0])
                 ys.append(p[1])
-            elif length == 4:
+            elif len(p) == 4:
                 bx, by = quadratic_bezier(xs[-1], ys[-1], p[0], p[1], p[2], p[3])
                 xs.extend(bx[1:])
                 ys.extend(by[1:])
             else:
-                raise ValueError(f"index {i}")
+                raise ValueError()
 
         self._shapes.append(self.Line(xs, ys))
 
+
+################
+### external ###
+################
 
 __d = __Drawer()
 # basics
@@ -190,9 +325,13 @@ config = __d.config
 plot = __d.plot
 save = __d.save
 
-# shapes
+# patched shapes
 circle = __d.circle
+
+# original shapes
+title = __d.title
 text = __d.text
+image = __d.image
 line = __d.line
 lines = __d.lines
 line_bezier = __d.line_bezier
--- tests/drawlib/test_core.py
+++ tests/drawlib/test_core.py
@@ -1,4 +1,5 @@
 from drawlib.core import *
+from PIL import Image, ImageFilter
 
 
 def test_circle():
@@ -8,11 +9,33 @@ def test_circle():
     save("test_circle.png")
 
 
+def test_title():
+    clear()
+    config(width=10, height=10, axis=True)
+    title("Test Title", y=-0.1)
+    save("test_title.png")
+
+
 def test_text():
     clear()
     config(width=10, height=10, axis=False)
     text(3, 3, "Hello World")
     save("test_text.png")
+
+
+def test_image_file():
+    clear()
+    config(width=10, height=10, axis=True)
+    image(1, 1, "test_icon.png", zoom=0.1)
+    save("test_image_file.png")
+
+
+def test_image_pil():
+    clear()
+    config(width=10, height=10, axis=True)
+    im = Image.open("test_icon.png")
+    image(1, 1, pilimg=im, zoom=0.1)
+    save("test_image_pil.png")
 
 
 def test_line():
@@ -29,9 +52,9 @@ def test_lines():
     save("test_lines.png")
 
 
-def test_line_curved():
+def test_line_bezier():
     clear()
     config(width=50, height=50, axis=True)
-    points = [(0, 30, 0, 20, 10, 20), (20, 20), (30, 20, 30, 10), (30, 20, 40, 20)]
-    line_bezier(points)
+    points = [(0, 20, 10, 20), (20, 20), (30, 20, 30, 10), (30, 20, 40, 20)]
+    line_bezier(0, 30, points)
     save("test_line_bezier.png")
