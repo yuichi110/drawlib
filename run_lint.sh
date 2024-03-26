92c8397f4c7697c32b426c14504987c841ca51d1
yuichi ito <yito86@gmail.com>
2024-03-27 01:14:41 +0900

    make typecheck script

--- run_lint.sh
+++ run_lint.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-set -e
 
 cd "$(dirname "$0")" || exit
 if command -v poetry &> /dev/null; then
     echo "$ poetry run pylint ./drawlib --rcfile=./drawlib/.pylintrc"
     poetry run pylint ./drawlib --rcfile=./drawlib/.pylintrc
+    echo
     echo "$ poetry run pylint ./tests --rcfile=./tests/.pylintrc"
     poetry run pylint ./tests --rcfile=./tests/.pylintrc
+    echo
     echo "$ poetry run pylint ./docs --rcfile=./docs/.pylintrc"
     poetry run pylint ./docs --rcfile=./docs/.pylintrc
 else
     echo "$ pylint ./drawlib --rcfile=./drawlib/.pylintrc"
     pylint ./drawlib --rcfile=./drawlib/.pylintrc
+    echo
     echo "$ pylint ./tests --rcfile=./tests/.pylintrc"
     pylint ./tests --rcfile=./tests/.pylintrc
+    echo
     echo "$ pylint ./docs --rcfile=./docs/.pylintrc"
     pylint ./docs --rcfile=./docs/.pylintrc
+    echo
 fi
--- run_typecheck.sh
+++ run_typecheck.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-set -e
 
 cd "$(dirname "$0")" || exit
 if command -v poetry &> /dev/null; then
-    poetry run mypy .
+    echo "$ poetry run mypy ./drawlib --disallow-untyped-defs"
+    poetry run mypy ./drawlib --disallow-untyped-defs
+    echo
+    echo "$ poetry run mypy ./tests"
+    poetry run mypy ./tests
+    echo
+    echo "$ run mypy ./docs"
+    poetry run mypy ./docs
 else
-    mypy .
+    echo "mypy ./drawlib --disallow-untyped-defs"
+    mypy ./drawlib --disallow-untyped-defs
+    echo
+    echo "mypy ./tests"
+    mypy ./tests
+    echo
+    echo "mypy ./docs"
+    mypy ./docs
 fi
